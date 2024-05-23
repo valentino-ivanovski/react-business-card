814bd78574dbb6f614b400a59bb850590e0f3a6b
Noah Raissi <coding@noahraissi.com>
2024-05-23 10:47:28 +0200

    inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Businesscard from './Businesscard';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Businesscard />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Businesscard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Businesscard.css';
+
+const Businesscard = () => {
+  return (
+    <div className="business-card">
+      Your business card
+    </div>
+  );
+};
+
+export default Businesscard;

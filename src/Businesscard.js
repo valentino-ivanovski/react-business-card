28792ec66b5a50ff7e274a3982bc8de1bf79728b
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2024-05-24 03:44:12 +0200

    Implemented the main elements of the card, implemented card hover animations from a component library, added basic css.

--- src/Businesscard.js
+++ src/Businesscard.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { CardContainer, CardBody, CardItem } from '/Users/tino/GIT/intern-react-challenge/src/components/ui/3d-card.tsx'; //I had to use the absolute path here because the relative path was not working for some reason.
 import './Businesscard.css';
 
 const Businesscard = () => {
   return (
-    <div className="business-card">
-      Your business card
-    </div>
+    <CardContainer>
+      <CardBody>
+        <CardItem className="card-item" translateZ={30}>
+          <div id='content'>
+            <img src="pfp.jpg" alt="profile-pic" />
+            <h1>Valentino Ivanovski</h1>
+            <h2>Computer Science <br></br>Student</h2>
+            <p>Phone: (+386) 69 646 407</p>
+            <p>GitHub: valentino-ivanovski</p>
+            <p>Address: Goriška Ulica 15, Koper, Slovenia</p>
+          </div>
+        </CardItem>
+      </CardBody>
+    </CardContainer>
   );
 };
 
